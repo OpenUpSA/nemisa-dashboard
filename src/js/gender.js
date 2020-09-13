b777214c705ac67653b0a17b8d6e1bb542024378
Adi Eyal <adi@openup.org.za>
2020-09-13 22:03:26 +0200

    Gender piechart now extends from PI chart

--- src/js/gender.js
+++ src/js/gender.js
@@ -1,8 +1,9 @@
 import * as dc from 'dc';
 import {d3} from './d3';
 
-export class GenderPie {
+export class GenderPie extends dc.PieChart {
     constructor(dataFilter, container) {
+        super(container);
         this.container = container;
         this.dimGender = dataFilter.dimensions['gender'].dimension;
         this.grpGender = this.dimGender.group().reduceCount();
@@ -10,21 +11,35 @@ export class GenderPie {
 
         this.prepareWidget(container);
         this.prepareDOM();
+
+        this.dimension(this.dimGender).group(this.grpGender);
     }
 
     prepareWidget(container) {
+        return
         this.chart = new dc.PieChart(container)
 
         this.chart
           .dimension(this.dimGender)
           .group(this.grpGender)
-          .colorCalculator((d, idx) => ({
-                0: "#CFCFCF",
-                1: "red"
-            }[idx]))
     }
 
     prepareDOM() {
         d3.select(this.container).select(".chart").style("display", "none");
+    }
+
+    _drawChart() {
+        super._drawChart()
+
+        this._g.each(function(d) {
+            if (d != undefined) {
+                const majorityAngle = Math.PI;
+                if (d.endAngle - d.startAngle >= majorityAngle)
+                    d3.select('path').classed('largest', true)
+                else
+                    d3.select('path').classed('largest', false)
+
+            }
+        })
     }
 }
