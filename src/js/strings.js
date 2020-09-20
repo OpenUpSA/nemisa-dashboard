3aa36cb0fd4b52e4c93f410acc69841b914c8340
Adi Eyal <adi@openup.org.za>
2020-09-20 05:32:23 +0200

    shortened question text

--- src/js/strings.js
+++ src/js/strings.js
@@ -1,71 +1,71 @@
-export const skillsExternalTraining = 'Do you get digital skills support from your company or organisation? [Digital skills in my organisation are promoted through external training]';
+export const skillsExternalTraining = 'training_external_training';
 export const mostUsedSkill = [
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Accounting/financial management]", label: "Accounting/financial management"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Make Bookings (Travel bookings, scheduling meetings, venue booking etc.)]", label: "Make bookings"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Emailing]", label: "Emailing"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Making audio or video calls]", label: "Audio/Video calls"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Marketing]", label: "Marketing"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Printing and scanning]", label: "Printing and scanning"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Designing]", label: "Designing"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Monitoring (e.g. employees, production, security etc)]", label: "Monitoring"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Research and development]", label: "Research and development"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Training]", label: "Training"},
-    {key: "How often do you use digital technologies to perform the following ACTIVITIES in your workplace? [Filing]", label: "Filing"}
+    {key: "activities_accounting", label: "Accounting/financial management"},
+    {key: "activities_booking", label: "Make bookings"},
+    {key: "activities_emailing", label: "Emailing"},
+    {key: "activities_audio_video_calls", label: "Audio/Video calls"},
+    {key: "activities_marketing", label: "Marketing"},
+    {key: "activities_printing", label: "Printing and scanning"},
+    {key: "activities_designing", label: "Designing"},
+    {key: "activities_monitoring", label: "Monitoring"},
+    {key: "activities_rnd", label: "Research and development"},
+    {key: "activities_training", label: "Training"},
+    {key: "activities_filing", label: "Filing"}
 ]
 export const skillsLearning = [
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [High school]", label: "High school"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [In-house training/ Workshops (training organised through internal processes)]", label: "In-house training/workshops"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [Peer-learning (learning from friends, colleagues, family member etc.)]", label: "Peer-learning"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [Self-taught]", label: "Self-taught"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [Short-courses]", label: "Short-courses"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [Tertiary education]", label: "Tertiary education"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [Previous work experience]", label: "Previous work experience"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [External training/workshops (training conducted and organised outside the organisation)]", label: "External training/workshops"},
-    {key: "How did you learn to use the digital technologies currently used in your organisation? Tick all that apply (More than one option can be selected). [I have NO digital skills]", label: "No digital skills"}
+    {key: "skills_high_school", label: "High school"},
+    {key: "skills_in_house", label: "In-house training/workshops"},
+    {key: "skills_peers", label: "Peer-learning"},
+    {key: "skills_self_taught", label: "Self-taught"},
+    {key: "skills_short_courses", label: "Short-courses"},
+    {key: "skills_tertiary", label: "Tertiary education"},
+    {key: "skills_previous_work", label: "Previous work experience"},
+    {key: "skills_external_training", label: "External training/workshops"},
+    {key: "skills_none", label: "No digital skills"}
 ]
 
 export const currentTech = [
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [State-of-the-art printers and scanners]", label: "State-of-the-art printers and scanners"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Business WhatsApp]", label: "Business WhatsApp"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Hi-tech telephone]", label: "Hi-tech telephone"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Faster computers and tablets]", label: "Faster computers and tablets"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Digital filing]", label: "Digital filing"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Mobile Applications]", label: "Mobile Applications"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Blockchain technology]", label: "Blockchain technology"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Reliable internet]", label: "Reliable internet"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Smartboards]", label: "Smartboards"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Software and web applications]", label: "Software and web applications"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Automated equipment]", label: "Automated equipment"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Voice over Internet protocol (VOIP)]", label: "VOIP"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Video conferencing applications]", label: "Video conferencing"},
-    {key: "Among the following digital technologies which ones do you currently use to perform your daily duties. [Artificial intelligence]", label: "Artificial Intelligence"}
+    {key: "daily_printers", label: "State-of-the-art printers and scanners"},
+    {key: "daily_whatsapp", label: "Business WhatsApp"},
+    {key: "daily_hitech_telephone", label: "Hi-tech telephone"},
+    {key: "daily_fast_computers", label: "Faster computers and tablets"},
+    {key: "daily_filing", label: "Digital filing"},
+    {key: "daily_mobile_apps", label: "Mobile Applications"},
+    {key: "daily_blockchain", label: "Blockchain technology"},
+    {key: "Adaily_internet", label: "Reliable internet"},
+    {key: "daily_smartboards", label: "Smartboards"},
+    {key: "daily_software", label: "Software and web applications"},
+    {key: "daily_automated_equipment", label: "Automated equipment"},
+    {key: "daily_voip", label: "VOIP"},
+    {key: "daily_video_conferencing", label: "Video conferencing"},
+    {key: "daily_ai", label: "Artificial Intelligence"}
 ]
 
 export const futureTech = [
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Faster Wi-Fi/Internet connection]", label: "Faster Internet"},
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Ultra-modern equipment (Upgrade current digital equipment/s)]", label: "Ultra-modern equipment"},
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Artificial Intelligence (AI)]", label: "Artificial Intelligence"},
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Internet of Things (IoT)]", label: "Internet of Things"},
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Robots]", label: "Robots"},
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Scanners]", label: "Scanners"},
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Tracking systems]", label: "Tracking Systems"},
-    {key: "In your opinion what are the other digital technologies you will require to perform your job in the future? If you are unsure, leave blank. [Voice over internet protocol (VOIP)]", label: "VOIP"}
+    {key: "future_fast_internet", label: "Faster Internet"},
+    {key: "future_ultra_moderm", label: "Ultra-modern equipment"},
+    {key: "future_ai", label: "Artificial Intelligence"},
+    {key: "future_iot", label: "Internet of Things"},
+    {key: "future_robots", label: "Robots"},
+    {key: "future_scanners", label: "Scanners"},
+    {key: "future_tracking_systems", label: "Tracking Systems"},
+    {key: "future_voip", label: "VOIP"}
 ]
 
 export const perceivedRisk = [
-    {key: "Perceived risk associated with digital technology usage in the workplace. [The use of digital technology exposes me to potential financial loss.]", label: "Potential financial loss"},
-    {key: "Perceived risk associated with digital technology usage in the workplace. [The probability of data loss is higher when using digital technology]", label: "Data loss"},
-    {key: "Perceived risk associated with digital technology usage in the workplace. [The possibility of getting scammed and robbed is higher when using digital technology]", label: "Fraud"},
-    {key: "Perceived risk associated with digital technology usage in the workplace. [Compared to traditional tools such as pen and paper, using digital technology creates more doubts]", label: "Creates doubts"},
-    {key: "Perceived risk associated with digital technology usage in the workplace. [It takes too much time to learn how to use new digital technology than using traditional tools.]", label: "Takes too much time"},
-    {key: "Perceived risk associated with digital technology usage in the workplace. [Digital technology is likely to cause me to lose control over the privacy of my personal data]", label: "Privacy issues"}
+    {key: "risks_financial_loss", label: "Potential financial loss"},
+    {key: "risks_data_loss", label: "Data loss"},
+    {key: "risks_scam", label: "Fraud"},
+    {key: "risks_doubts", label: "Creates doubts"},
+    {key: "risks_time", label: "Takes too much time"},
+    {key: "risks_privacy", label: "Privacy issues"}
 ]
 
 export const attitudes = [
-    {key: "What is your attitude toward using digital technologies? [The usage of digital technologies in the workplace is a waste of time]", label: "Waste of time"},
-    {key: "What is your attitude toward using digital technologies? [I am NOT interested in using digital skills in my workplace]", label: "Not interested"},
-    {key: "What is your attitude toward using digital technologies? [I don’t see any additional value in using digital technology]", label: "No additional value"},
-    {key: "What is your attitude toward using digital technologies? [The usage of digital technology is too complicated for me]", label: "Too complicated"},
-    {key: "What is your attitude toward using digital technologies? [I can still use my traditional tools, there is no real need for digital technologies]", label: "No real need"},
-    {key: "What is your attitude toward using digital technologies? [Digital technologies limit my capacity as a human because everything is provided by machines]", label: "Limits human capacity"}
+    {key: "usage_waste_of_time", label: "Waste of time"},
+    {key: "usage_not_interested", label: "Not interested"},
+    {key: "usage_no_value", label: "No additional value"},
+    {key: "usage_too_complicated", label: "Too complicated"},
+    {key: "usage_traditional_tools", label: "No real need"},
+    {key: "usage_limit_capacity", label: "Limits human capacity"}
 ]
