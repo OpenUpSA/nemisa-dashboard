0a2712ba6aab665ef5d70cb8cba67d6d97dc5891
Adi Eyal <adi@openup.org.za>
2020-09-23 11:22:29 +0200

    Updated post.py to add missing industry

--- data/post.py
+++ data/post.py
@@ -1,11 +1,23 @@
 import requests
 import json
+import csv
 
 hostname = "http://localhost:8000/api/responses/"
-hostname = "https://k4i.openup.org.za"
+# hostname = "https://k4i.openup.org.za"
+
+reader = csv.DictReader(open("Employees - cleaned.csv"))
+employees_data = [row for row in csv.DictReader(open("Employees - cleaned.csv"))]
+employees_js = [js for js in json.load(open("data.json"))]
+industries = {row["Sector"]: row for row in csv.DictReader(open("Industry codes.csv"))}
+
+assert len(employees_js) == len(employees_data)
 
 headers = {"content-type": "application/json;charset=utf-8"}
-for js in json.load(open("data.json")):
+for (raw_data, js) in zip(employees_data, employees_js):
+    industry_code = raw_data["Industry code"]
+    print(industries)
+    industry = industries[industry_code]
+    js["Industry"] = industry
     #data = {"data": js}
     data = js
     r = requests.post(hostname, headers=headers, data=json.dumps(data))
