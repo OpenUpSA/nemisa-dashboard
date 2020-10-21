880949335443999636614f2bc0b507a345a7e705
Adi Eyal <adi@openup.org.za>
2020-10-21 13:09:51 +0200

    Added mechanism for validation warnings
    Removed default text because the unordered list ended up being displayed inline with it

--- assets/js/survey/sections/base.js
+++ assets/js/survey/sections/base.js
@@ -10,14 +10,32 @@ export class Section {
 
     prepareDOM() {
         this.block.select("h3").text(this.title);
+        this.warningContainer = this.block.select('.form-warning');
     }
 
     appendChild(element) {
-        const warningContainer = this.block.select('.form-warning')
-        this.block.node().insertBefore(element, warningContainer.node());
+        this.block.node().insertBefore(element, this.warningContainer.node());
     }
 
     get node() {
         return this.block.node();
+    }
+
+    displayWarnings(messages) {
+        this.hideWarnings();
+        this.warningContainer.append("ul")
+            .selectAll("li")
+            .data(messages)
+            .enter()
+                .append("li")
+                .text(d => d)
+
+        this.warningContainer.classed('hidden', false);
+    }
+
+    hideWarnings() {
+        this.warningContainer.selectAll("*").remove()
+        this.warningContainer.classed('hidden', true);
+
     }
 }
