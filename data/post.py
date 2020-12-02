85573978cacc8542b0a00d39fcbe006371457c79
“rikusv” <rikusv@gmail.com>
2020-12-02 22:08:11 +0200

    Change post script to to multiple surveys

--- data/post.py
+++ data/post.py
@@ -2,24 +2,37 @@ import requests
 import json
 import csv
 
-hostname = "http://localhost:8000/api/responses/?survey=1"
-# hostname = "https://k4i.openup.org.za"
+url = 'http://localhost:8000/api/responses/'
+# hostname = 'https://k4i.openup.org.za'
+headers = {'content-type': 'application/json;charset=utf-8'}
 
-reader = csv.DictReader(open("Employees - cleaned.csv"))
-employees_data = [row for row in csv.DictReader(open("Employees - cleaned.csv"))]
-employees_js = [js for js in json.load(open("data.json"))]
-industries = {row["Sector"]: row for row in csv.DictReader(open("Industry codes.csv"))}
-
-assert len(employees_js) == len(employees_data)
-
-headers = {"content-type": "application/json;charset=utf-8"}
-for (raw_data, js) in zip(employees_data, employees_js):
-    industry_code = raw_data["Industry code"]
-    industry = industries[industry_code]["Description"]
-    js["data"]["Industry"] = industry
+def __post(survey, data):
     body = {
-        "survey": 1,
-        "data": js["data"]
+        'survey': survey,
+        'data': data
     }
-    r = requests.post(hostname, headers=headers, data=json.dumps(body))
+    r = requests.post(url, headers=headers, data=json.dumps(body))
     print(r.status_code)
+
+def survey1():
+    reader = csv.DictReader(open('Employees - cleaned.csv'))
+    employees_data = [row for row in csv.DictReader(open('Employees - cleaned.csv'))]
+    employees_js = [js for js in json.load(open('data.json'))]
+    industries = {row['Sector']: row for row in csv.DictReader(open('Industry codes.csv'))}
+
+    assert len(employees_js) == len(employees_data)
+
+    for (raw_data, js) in zip(employees_data, employees_js):
+        industry_code = raw_data['Industry code']
+        industry = industries[industry_code]['Description']
+        js['data']['Industry'] = industry
+        __post(1, js['data'])
+
+def survey2():
+    with open('data-2.json') as file:
+        records = json.load(file)
+    for record in records:
+        __post(2, record)
+
+# survey1()
+survey2()
