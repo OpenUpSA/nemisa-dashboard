685243d6d7097e60a706708b51bf7b90578beeaa
“rikusv” <rikusv@gmail.com>
2020-12-02 22:05:54 +0200

    Remove unneeded code

--- k4i_dashboard/survey/serializers.py
+++ k4i_dashboard/survey/serializers.py
@@ -14,8 +14,3 @@ class SurveyResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = SurveyResponse
         fields = ('survey', 'data',)
-
-    def create(self, data):
-        survey = self.context['request'].query_params.get('survey', None)
-        data['survey'] = Survey.objects.get(id=survey)
-        return SurveyResponse.objects.create(**data)
