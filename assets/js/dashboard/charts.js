37dedb0e638f5e018f48fd2ea4841597a20c69fc
“rikusv” <rikusv@gmail.com>
2020-12-04 21:57:57 +0200

    Fix

--- assets/js/dashboard/charts.js
+++ assets/js/dashboard/charts.js
@@ -1,12 +1,11 @@
 export class Charts {
   constructor($grid, dataFilter, charts) {
     const $chartTemplate = $grid.find('.block').first().clone(true);
-    debugger;
     $grid.empty();
     charts.forEach((chart) => {
       const $block = $chartTemplate.clone(true);
       $grid.append($block);
-      new chart.Class($block[0], chart.dimensionName, dataFilter, chart.sortOrder);
+      new chart.Class($block.find('.chart-embed')[0], chart.dimensionName, dataFilter, chart.sortOrder);
     });
   }
 }
