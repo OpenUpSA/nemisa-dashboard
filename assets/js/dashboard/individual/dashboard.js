361569e44f32f083be5e0edb1cac0f46b4ad5f88
“rikusv” <rikusv@gmail.com>
2021-06-28 13:41:06 +0200

    Revert "Change icon"
    This reverts commit a8cf2862378a43574a6c0b988975c7b68b695679.

--- assets/js/dashboard/individual/dashboard.js
+++ assets/js/dashboard/individual/dashboard.js
@@ -69,7 +69,7 @@ export default function individualDashboard($container, data) {
     {
       Class: RowChart,
       dimensionName: 'internetUsage',
-      icon: 'calendar',
+      icon: 'chart-bar',
     },
     {
       Class: RowChart,
